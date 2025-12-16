07574b293be491ed158d60d315aefb187bc05da6
aozodbek2197 <aozodbek2197@gmail.com>
2025-12-16 15:48:16 +0500

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,17 @@
+-- 1-mashq
+INSERT INTO Talabalar (id, ism, yosh, gpa)
+VALUES
+(1, 'Ali', 20, 3.5),
+(2, 'Vali', 21, 3.8),
+(3, 'Sardor', 19, 3.2);
+-- 2-mashq
+SELECT * FROM Mahsulotlar;
+-- 3-mashq
+SELECT nom, narx
+FROM Mahsulotlar;
+-- 4-mashq
+SELECT gpa
+FROM Talabalar;
+-- 5-mashq
+SELECT *
+FROM Xodimlar;
